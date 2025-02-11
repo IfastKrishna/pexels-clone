64b8e133ef6d973b9506b98d2ab3d8aae6d1a52f
krishna <ifastkrishna@gmail.com>
2025-02-11 18:04:00 +0530

    done every thing

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://api.pexels.com/",
   headers: {
-    Authorization: `Z6OvRwIFp0ivGogCdePfz0g1bR9BwlRGluTD4ifSU7EYyhuWcCAh8lDA`,
+    Authorization: `${process.env.NEXT_PUBLIC_PEXELS_API_KEY}`,
   },
 });
 
